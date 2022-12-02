87b8c2dfd7b4c5eb73529c6e8a27a8638fba0b66
Jonas Bette <jonas.bette@student.uclouvain.be>
2022-12-02 11:13:30 +0100

    Ajout des sessoins, du serveur en https, des modules nécessaires ainsi que les clés

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
+/*
+*   note à soi-même, si besoin de faire un changement dans le serveur,
+*   pour le relancer plus facilement, faire ctrl + c
+*/
+
+// chargement des prérequis
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+var session = require('express-session');
+var https = require('https');
+var fs = require('fs');
 
+// mise en place
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/static');
 app.use(express.static(__dirname + "/static"))
 
+// chargement et utilisation des sessions
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: "JonasThomasChristos",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        path: '/',
+        httpOnly: true,
+    }
+}));
+
+// page principale
 app.get('/', function (req, res) {
     res.render('home.ejs')
 });
 
-app.listen(8080);
+// lancement du server
+https.createServer({
+    key: fs.readFileSync(__dirname + '/static/key/key.pem'),
+    cert: fs.readFileSync(__dirname + '/static/key/cert.pem'),
+    passphrase: 'ingi'
+}, app).listen(8080);
 console.log('Server connected');
+console.log('Go to https://localhost:8080');
